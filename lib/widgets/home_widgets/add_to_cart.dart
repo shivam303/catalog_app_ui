ebfe21b55164a38844ecd31ad58ae3c56e683341
shivam303 <shivamsachan303@gmail.com>
2021-06-04 17:24:07 +0530

    first commit

---
+++ lib/widgets/home_widgets/add_to_cart.dart
@@ -0,0 +1,35 @@
+import 'package:first/core/store.dart';
+import 'package:first/models/cart.dart';
+import 'package:first/models/catalog.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:velocity_x/velocity_x.dart';
+
+class AddToCart extends StatelessWidget {
+  final Item catalog;
+  AddToCart({
+    Key key,
+    this.catalog,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    VxState.listen(context, to: [AddMutation, RemoveMutation]);
+    final CartModel _cart = (VxState.store as MyStore).cart;
+    bool isInCart = _cart.items.contains(catalog) ?? false;
+    return ElevatedButton(
+      onPressed: () {
+        if (!isInCart) {
+          AddMutation(catalog);
+        }
+      },
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(context.theme.buttonColor),
+          shape: MaterialStateProperty.all(
+            StadiumBorder(),
+          )),
+      child: isInCart ? Icon(Icons.done) : Icon(CupertinoIcons.cart_badge_plus),
+    );
+  }
+}
